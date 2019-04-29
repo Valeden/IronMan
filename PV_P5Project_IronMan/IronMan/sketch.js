5ca94f93933b675d14bdc3a7f05a61b8f28325a7
Valeden <49344334+Valeden@users.noreply.github.com>
2019-04-29 15:08:57 -0400

    Update sketch.js

--- PV_P5Project_IronMan/IronMan/sketch.js
+++ PV_P5Project_IronMan/IronMan/sketch.js
@@ -1,4 +1,5 @@
-var opp = 255; // variable will be used for opacity
+var opp = 0; // variable will be used for opacity
+var sOpp = 0; // variable will be used for stroke opacity
 
 // The setup function only happens once
 function setup() {
@@ -42,18 +43,14 @@ function draw() {
       fill("#AE1FB9"); // power purple
       ellipse(450,375,50,100); // power stone
 	    
-      fill("#000000"); // text color
+      fill(0, 0, 0, opp); // text color
       textSize(40);
       textFont("Times New Roman");
       textStyle(BOLD);
       textAlign(CENTER);
-      strokeWeight(2); // text outline
-      stroke("#B97D10"); // gold outline
+      strokeWeight(sOpp); // text outline
+      stroke(185, 125, 16); // gold outline
       text("ILY3K", 250, 90);
-	    
-      fill(106, 12, 11, opp); // box color(red)
-      strokeWeight(0); // outline off
-      rect(190, 55, 150, 50); // box to cover up text
     }
   else
     {
@@ -63,7 +60,7 @@ function draw() {
       fill("#848482"); // chestpiece no light
       ellipse(250, 250, 275, 275); // chestpiece
 	    
-      fill("#000000"); // text color
+      fill(0, 0, 0, opp); // text color
       textSize(40);
       textFont("Times New Roman");
       textStyle(BOLD);
@@ -71,18 +68,25 @@ function draw() {
       strokeWeight(0); // outline off
       text("ILY3K", 250, 90);
 	    
-      fill(106, 12, 11, opp); // box color(red)
-      rect(190, 55, 150, 50); // box to cover up text
     }
   
   if (keyIsPressed == true)
-    {
-      opp = opp - 5
-    }
+  {
+      opp = opp + 255
+  }
 	
-  if (opp < 0)
-    {
-      opp = 255
-    }
-    
+  if (opp > 255)
+  {
+      opp = 0
+  }
+  
+  if (opp == 255)
+  {
+    sOpp = 2
+  }
+  
+  else
+  {
+    sOpp = 0
+  }
 }
